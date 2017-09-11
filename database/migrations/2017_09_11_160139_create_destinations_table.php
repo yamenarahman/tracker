62645b2ab327b90c3fb3e77d7140f272231f41c3
zlegend-210 <yamen.a.abdulrahman@hotmail.com>
2017-09-11 18:53:49 +0200

    initial commit

---
+++ database/migrations/2017_09_11_160139_create_destinations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDestinationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->index();
+            $table->string('address')->nullable();
+            $table->string('longitude');
+            $table->string('latitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destinations');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-info">
+                <div class="panel-heading">Current location</div>
+
+                <div class="panel-body">
+                    <div id="map" style="height:400px; "></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@push('js')
+<script>
+var im = 'https://www.robotwoods.com/dev/misc/bluecircle.png'; //'{{ asset('tracker-icon.png') }}'
+function initMap() {
+        // Initialize the Google Maps API v3
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 15,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var marker = null;
+
+function autoUpdate() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var newPoint = new google.maps.LatLng(position.coords.latitude,
+                                          position.coords.longitude);
+
+    if (marker) {
+      // Marker already created - Move it
+      marker.setPosition(newPoint);
+    }
+    else {
+      // Marker does not exist - Create it
+      marker = new google.maps.Marker({
+        position: newPoint,
+        map: map,
+        icon: im
+      });
+    }
+
+    // Center the map on the new position
+    map.setCenter(newPoint);
+  });
+
+  // Call the autoUpdate() function every 5 seconds
+  setTimeout(autoUpdate, 5000);
+}
+
+autoUpdate();
+}
+</script>
+@endpush
